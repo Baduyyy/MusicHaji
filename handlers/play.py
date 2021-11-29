2346962efe209c06b06a127834097d647675a49b
Muhammad Fikrul Alam <89534928+Baduyyy@users.noreply.github.com>
2021-11-29 09:55:34 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -134,7 +134,7 @@ async def nplay(_, message: Message):
     for i in message.command[1:]:
         query += " " + str(i)
     print(query)
-    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    ydl_opts = {"format": "bestaudio/best"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
         url = f"https://youtube.com{results[0]['url_suffix']}"
